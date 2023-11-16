d2332cc53137bfb1179ce430c195e1edcd695d17
Pia Merete Eriksen <piamer@uio.no>
2023-11-16 11:28:52 +0100

    edited DB

--- DB_and_Genotype_calling.sh
+++ DB_and_Genotype_calling.sh
@@ -25,17 +25,23 @@ module load BIOS-IN5410/HT-2023
 ## this creates a text file with all the HaplotypeCalled.gvcf.gz filenames.
 ls *gvcf.gz > gvcf.list
 
-##force create and remove an empty directory (needed to prevent errors when rerunning)
+## force create and remove an empty directory (needed to prevent errors when rerunning)
 mkdir -p ${FILENAME}_DB; rm -r ${FILENAME}_DB
 
-##run GATK database import (2nd step)
+## run GATK database import (2nd step)
 gatk GenomicsDBImport -V gvcf.list \
 --genomicsdb-workspace-path ${FILENAME}_DB \
 --intervals NC_004029.2
 
-##run GATK genotype GVCF (3rd step)
+## run GATK genotype GVCF (3rd step)
 gatk GenotypeGVCFs -R ../Orosv1mt.fasta \
 -V gendb://${FILENAME}_DB -O ${FILENAME}.vcf.gz
+
+## Access to PCA program SMART PCA
+export PATH="/projects/ec34/biosin5410/sbatch_intro/SNP_calling/script/:$PATH"/
+
+## Run PCA program SMART PCA
+Run_PCA ${FILENAME}.vcf.gz
 
 ## Message that you are done with the job
 echo "Finished running jobs"
